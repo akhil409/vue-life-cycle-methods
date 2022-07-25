f8bb7f595c0d1ba29358218c22fd779fdc3e547b
akhil409 <akhil.alladi9@.com>
2022-07-25 10:50:00 +0530

    Vue-Life-Cycle-Methods

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <div v-if="display">
+            <LifeCycleMethods v-bind:hookData="hookData" v-bind:text="text"/>
+        </div> <br>
+        <button v-on:click="deleteComponent">
+            <span v-if="display">Destroy Component</span>
+            <span v-else>Restore Component</span>
+        </button>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LifeCycleMethods from './components/LifeCycleMethods.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    LifeCycleMethods,
+},
+    data(){
+      return{
+           hookData:1,
+           text:"Welcome to Life Cycle Hooks Page",
+           display:true,
+           data:[],
+      }
+    },
+    methods:{
+        deleteComponent(){
+            this.display=!this.display
+        }
+    },
+    beforeCreate(){
+       alert('beforeCreate Hook has been called')
+       console.log('beforeCreate Hook has been called');
+    },
+    created(){
+         alert('Created Hook has been called')
+         console.log('Created Hook has been called',this.hookData,this.text);
+        //  axios.get('https://jsonplaceholder.typicode.com/posts').then( res => this.data = res)
+    },
+    beforeMount(){
+         alert('beforeMount Hook has been called')
+         console.log('beforeMount Hook has been called');
+    },
+    mounted(){
+         alert('Mounted Hook has been called')
+        console.log('Mounted Hook has been called',this.hookData,this.text);
+        this.$data.text='Pleasure is Mine....!';
+
+    },
+    beforeUpdate(){
+         alert('beforeUpdate Hook has been called')
+        console.log('beforeUpdate Hook has been called');
+    },
+    updated(){
+         alert('Updated Hook has been called')
+        console.log('Updated Hook has been called',this.hookData,this.text);
+    },
+    // beforeDestroy(){
+    //     alert('beforeDestroy Hook has been called')
+    //     console.log('beforeDestroy Hook has been called');
+    //     // this.hookData=null
+    //     // delete this.hookData
+    // },
+    // destroyed(){
+    //      alert('Destroyed Hook has been called');
+    //      console.log('Destroyed Hook has been called');
+    //     //  this.$destroy();0
+    //     //  console.log(this)
+    // }
 }
 </script>
 
---
+++ src/components/LifeCycleMethods.vue
@@ -0,0 +1,42 @@
+<template>
+      <div>
+          <h1>Life cycle Hooks Component</h1>
+          <button @click="hookData++">Increment</button>
+          {{hookData}}   <!--{{increment}} -->
+          
+          <button v-on:click="text=!text">
+            <span v-if="text" >Hide</span>
+            <span v-else >Show</span>
+          </button>
+          {{text}}
+      </div>
+</template>
+
+<script>
+export default {
+       name:'LifeCycleMethods',
+       props:['hookData','text'],
+       data(){
+            return{
+              increment:0,
+            }
+       },
+      //  methods:{
+      //        incremented(){
+      //          return this.hookData++
+      //        }
+      //  },
+       beforeDestroy(){
+        alert('beforeDestroy Hook has been called')
+        console.log('beforeDestroy Hook has been called');
+       },
+       destroyed(){
+         alert('Destroyed Hook has been called')
+         console.log('Destroyed Hook has been called');
+       }
+}
+</script>
+
+<style>
+
+</style>
